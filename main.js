997da388319a7c21a5f25bc9dff16647a4c8858f
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-08-03 15:37:12 -0400

    changes the text

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const paragraph = document.querySelector('p')
+//changes the color of the paragraph
 paragraph.style.color = 'lightblue'
+// changes the text of the paragraph
+paragraph.innerText = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpresss'
+
 
 const heading = document.querySelector('h1')
-heading.style.fontSize ='5 em'
+//changes the size of the font for the header
+heading.style.fontSize ='5em'
+
